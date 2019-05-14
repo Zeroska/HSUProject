e2f7c0c8e85fe255783611161bcd9744e960d4aa
Zeroska <owlthres@gmail.com>
2019-05-14 11:27:01 +0700

    Project 1

---
+++ HSUProject.c
@@ -0,0 +1,37 @@
+#include "stdio.h"
+
+
+#define NGAY_DEFAULT 11
+#define THANG_DEFAULT 5
+#define NAM_DEFAULT 2019
+
+
+//3 truong hop tinh do chenh lech thang
+//Truong hop 1: ThangNguoiDungNhap == THANG_DEFAULT && Cung Nam
+//Truong hop 2: ThangNGuoiDUngNhap != THANG_DEFAULT && Khac Nam
+//Truong hop 3: ThangNguoiDungNhap < THANG_DEFAULT && namNguoiDungNhap vao <NAM_DEFAULT
+
+//truyen thang va nam, check 
+
+int tinhDoChenhLechCuaThang(int thangNguoiDungNhap, int namNguoiDungNhap)
+{
+    //check
+    
+    if (thangNguoiDungNhap < THANG_DEFAULT && namNguoiDungNhap == NAM_DEFAULT)
+    {
+        
+    }
+    else
+    {
+        if()
+        {
+
+        }
+    }
+}
+
+
+int main()
+{
+
+}
